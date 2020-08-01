aa8e03d876fcd4ad0fead4bd1b8fdbe9db566d16
AmmarCode <ammar.code01@gmail.com>
2020-08-01 14:09:57 -0700

    Updated App.py

--- App.py
+++ App.py
@@ -1,5 +1,6 @@
 import constants
 import copy
+import sys
 
 
 def clean_data():
@@ -19,13 +20,13 @@ def balance_team():
     # Devide players to 2 groups: Experienced, Inexperienced
     experienced = [player for player in players if player['experience'] == True]
     inexperienced = [player for player in players if player['experience'] == False]
-    team_total = len(experienced)/ len(teams) + len(inexperienced)/ len(teams)
+    team_total = len(experienced) / len(teams) + len(inexperienced) / len(teams)
     # Add equal number of experienced players to each team
     for player in experienced:
         if player not in Panthers and len(Panthers) != 3:
             Panthers.append(player)
         elif player not in Bandits and len(Bandits) != 3:
-              Bandits.append(player)
+            Bandits.append(player)
         elif player not in Warriors and len(Warriors) != 3:
             Warriors.append(player)
     # Add equal number of inexperienced players to each team
@@ -33,24 +34,18 @@ def balance_team():
         if player not in Panthers and len(Panthers) != 6:
             Panthers.append(player)
         elif player not in Bandits and len(Bandits) != 6:
-              Bandits.append(player)
+            Bandits.append(player)
         elif player not in Warriors and len(Warriors) != 6:
             Warriors.append(player)
     # Add team names to the team's lists
-    Panthers[0] = {'team name': 'Panthers', 'name': 'Karl Saygan',
-                   'guardians': ['Heather Bledsoe'], 'experience': True,
-                   'height': 42}
-    Bandits[0] = {'team name': 'Bandits', 'name': 'Les Clay',
-                  'guardians': ['Wynonna Brown'], 'experience': True,
-                  'height': 42}
-    Warriors[0] = {'team name': 'Warriors', 'name': 'Herschel Krustofski',
-                   'guardians': ['Hyman Krustofski ', ' Rachel Krustofski'],
-                   'experience': True, 'height': 45}
+    Panthers[0]['team name'] = teams[0]
+    Bandits[0]['team name'] = teams[1]
+    Warriors[0]['team name'] = teams[2]
 
 
 def menu():
     # Print App name and menu options
-    print(f"{'-'*26}\nBASKETBALL TEAM STATS TOOL\n{'-'*26}\n\n----MENU----\n\n"
+    print(f"{'-' * 26}\nBASKETBALL TEAM STATS TOOL\n{'-' * 26}\n\n----MENU----\n\n"
           "Here are your choices:\n1) Display Team Stats\n2) Quit")
     while True:
         try:
@@ -84,28 +79,30 @@ def menu():
             if choice == 2:
                 # Print Goodbye message
                 print(f"{'-' * 46}\nThank you for using BASKETBALL TEAM STATS TOOL\n{'-' * 46}")
-                break
+                sys.exit()
 
 
 def display_stats(team):
-
+    count = 0
     while True:
         # Calculate average height
         try:
             team_height = [player['height'] for player in team]
             sum_height = sum(team_height)
-            avg_height =round((sum_height) / len(team), 1)
+            avg_height = round((sum_height) / len(team), 1)
         except TypeError:
+            print(count)
+            count += 1
             continue
         # display team stats:
         # ( Total players, Experienced, Inexperienced, Average height
-        print(f"\nTeam: {team[0]['team name']}\n{'-'*20}\nTotal players: {len(team)}\nTotal "
-        f"experienced: {int(len(team) / 2)}\nTotal inexperienced: "
-        f"{int(len(team) / 2)}\nAverage height: {avg_height}\n\nPlayers on team:")
+        print(f"\nTeam: {team[0]['team name']}\n{'-' * 20}\nTotal players: {len(team)}\nTotal "
+              f"experienced: {int(len(team) / 2)}\nTotal inexperienced: "
+              f"{int(len(team) / 2)}\nAverage height: {avg_height}\n\nPlayers on team:")
         # Format players name to string
-        name_list = [player['name']for player in team]
+        name_list = [player['name'] for player in team]
         name = ', '.join(name_list)
-        print(name+'\n')
+        print(name + '\n')
         # Format guardians to string
         guardians = []
         for index in team:
@@ -117,7 +114,8 @@ def display_stats(team):
         input("Press Enter to continue")
         display_stats(menu())
 
-#------------------------------------------------
+
+# ------------------------------------------------
 if __name__ == '__main__':
     players = copy.deepcopy(constants.PLAYERS)
     teams = copy.deepcopy(constants.TEAMS)
